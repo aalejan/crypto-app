9b13e8b12247e9ba68aba40480e158cdb4f7641f
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-02-22 00:57:44 -0500

    fixed alignment of contents within each coin list item

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -8,17 +8,20 @@ export const Coin = ({coin, deleteCoin, noIcon}) => {
     return (
       <Link className="text-decoration-none" to={`/coins/${coin.id}`} >
         <li className="coilist-item list-group-item list-group-item-action d-flex justify-content-between align-items-center rounded ">
-          <span ><img src={coin.image} alt="" className="coinlist-image "/>  {coin.name}</span>
+          <div className="container row">
+             <span className="col-sm" ><img src={coin.image} alt="" className="coinlist-image "/>  {coin.name}</span>
          
-          <span className="text-decoration-none">{`$${coin.current_price}`}</span>
-          <span className={coin.price_change_percentage_24h < 0 ? 'text-danger mr-2' : 'text-success'}>
-            <i className={coin.price_change_percentage_24h < 0 ? 'fas fa-sort-down align-middle mr-1' : 'fas fa-sort-up align-middle mr-1'}></i>
+          <span className="text-decoration-none col-sm text-center">{`$${coin.current_price}`}</span>
+          <span className={coin.price_change_percentage_24h < 0 ? 'text-danger mr-2 col-sm d-flex justify-content-end' : 'text-success col-sm d-flex justify-content-end'}>
+            <i className={coin.price_change_percentage_24h < 0 ? 'fas fa-sort-down align-middle mr-1 col-sm d-flex justify-content-end' : 'fas fa-sort-up align-middle mr-1 col-sm d-flex justify-content-end'}></i>
             {coin.price_change_percentage_24h}
           </span>
           <i onClick={e => {
             e.preventDefault()
             deleteCoin(coin.id)
-          }} className={noIcon ? "" :"delete-icon far fa-times-circle text-danger" }></i>
+          }} className={noIcon ? "" :"delete-icon far fa-times-circle text-danger col-sm d-flex justify-content-end" }></i>
+          </div>
+         
         </li>
       </Link>
     )
